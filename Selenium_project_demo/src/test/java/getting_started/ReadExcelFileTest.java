56437c40b783cdc4c9dbeaa34a7aa3bb06e8a9d7
Kyle O'Keeffe <78436677+kyleokeeffe@users.noreply.github.com>
2022-08-03 16:09:04 -0400

    Added some methods for parsing/searching imported excel table

--- Selenium_project_demo/src/test/java/getting_started/ReadExcelFileTest.java
+++ Selenium_project_demo/src/test/java/getting_started/ReadExcelFileTest.java
@@ -1,34 +1,63 @@
 package getting_started;
 
 import org.junit.Test;
-import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Iterator;
 
-public class ReadExcelFileTest {
+public class ReadExcelFileTest{
 
+	private static XSSFSheet sheet;
 	@Test
 	public void test() {
 		String fileName = "ClientTestData.xlsx";
-		
 		try {
 			File excelFile = new File(System.getProperty("user.dir")+ "\\testData\\" + fileName);
 			FileInputStream fileSystem = new FileInputStream(excelFile);
-		    XSSFWorkbook book = new XSSFWorkbook(fileSystem);
-		    XSSFSheet sheet  = book.getSheetAt(0);
-		    
-		    System.out.println(sheet.getRow(0).getPhysicalNumberOfCells());		
-		    
-		    XSSFRow tableHeader = sheet.getRow(0);
+			XSSFWorkbook book = new XSSFWorkbook(fileSystem);
+		    sheet  = book.getSheetAt(0);
+		
+		    for( Iterator<Row> i = sheet.iterator(); i.hasNext();) {//perform this action for every row
+				Row iRow = i.next();
+				String cellValueAtCol = GetCellByHeader(iRow,"ClientLastName").getStringCellValue();//to attain value for passing to driver.findelement().sendkeys() for specified column name
+				System.out.println(cellValueAtCol);
+		    }
 		}
 		catch(Exception e) {
 			System.out.println("error was: " +e.getMessage());
-			
 		}
-		
 		//fail("Not yet implemented");
 	}
-
+	
+	//public static void GetRowByValueAtColumn(string headerValue, string)
+	
+	public static Cell GetCellByHeader(Row thisRow,String headerValue) {
+		return thisRow.getCell(GetHeaderColNum(headerValue));
+	}
+	
+	public static Integer GetHeaderColNum(String searchString) {
+		Row headerRow = sheet.getRow(0);
+		Integer colNum=null;
+	    	 for( Iterator<Cell> j = headerRow.iterator(); j.hasNext();) {
+		    	Cell iCell = j.next();
+	    		if(iCell.getStringCellValue().equals(searchString)) {
+		    		colNum = iCell.getColumnIndex();
+		    		break;		
+		    	 }
+	    	 }
+	    	if(colNum.equals(null))
+	    		System.out.println("No match found for " + searchString);
+	   return colNum;
+	}
 }
+	
+
+	
+	
+	
+	
+
